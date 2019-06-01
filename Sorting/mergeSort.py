be509dd50274ad378f09473bf15eaa8c84940b03
HHHHHp <406149488@qq.com>
2019-06-01 19:56:40 +0800

    add comment about merge sort

--- Sorting/mergeSort.py
+++ Sorting/mergeSort.py
@@ -5,9 +5,12 @@ def merge(l1,l2,arr):
     i = j = 0
     while i+j<len(arr):
         if j==len(l2) or (i<len(l1) and l1[i] < l2[j]):
+            # 如果第二个表走完了，或者第一个表没走完且l1[i] < l2[j]
+            # 则表示还得比较两个表，且l1[i]比l2[j]更小，则新加入arr的元素为l1[i]
+            
             arr[i+j] = l1[i]
             i += 1
-        else:
+        else:   # l1走完了或者都没走完l2[j]<l1[i] 则添加l2[j]到arr
             arr[i+j] = l2[j]
 
             j += 1
